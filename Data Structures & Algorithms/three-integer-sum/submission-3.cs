c9818d069d712379676cd743f1bab895f2229cc3
yaho391 <poiuyt290616@gmail.com>
2026-09-03 15:13:55 +0300

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        var result = new List<List<int>>();
+        
+        Array.Sort(nums);
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            if(i > 0 && nums[i] == nums[i-1])
+                    continue;
+            
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0)
+                {
+                    result.Add(new List<int> {nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                    {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1])
+                    {
+                        right--;
+                    }
+                }
+                else if(sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+
+            }
+        }
+
+        return result;
+    }
+}
